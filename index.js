6475757fae6f3c42908b898c5872e065ebd644d5
sidee <said.issack@student.moringaschool.com>
2023-06-13 00:04:23 +0300

    completed assighnment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+const companyName = "Scuber";
+let mostProfitableNeighborhood = "Chelsea";
+let companyCeo = "Susan Smith";
+
+console.log("The company name is", companyName);
+console.log("The most profitable neighborhood is", mostProfitableNeighborhood);
+console.log("The company CEO is", companyCeo);
+
